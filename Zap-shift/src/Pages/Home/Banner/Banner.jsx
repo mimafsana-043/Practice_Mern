87ab547eae6c65c0820ef450c7f417b77ce7a942
Mimnoob <hamza197124@gmail.com>
2026-03-12 19:45:07 +0600

    banner added

--- Zap-shift/src/Pages/Home/Banner/Banner.jsx
+++ Zap-shift/src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,25 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import banner1 from '../../../assets/banner/banner1.png';
+import banner2 from '../../../assets/banner/banner2.png';
+import banner3 from '../../../assets/banner/banner3.png';
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <Carousel>
+                <div>
+                    <img src={banner1} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={banner2} />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src={banner3} />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
     );
 };
 
